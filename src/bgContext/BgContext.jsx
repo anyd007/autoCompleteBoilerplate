e1b4287719226d3af87c6392b651236af5d0710c
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-05-17 20:20:24 +0200

    stylowanie

--- src/bgContext/BgContext.jsx
+++ src/bgContext/BgContext.jsx
@@ -1,10 +1,15 @@
 import React, { useState, createContext, useContext } from "react";
 const themes = {
   dark: {
+    placeholder:{
+    color:"#F8D800",
+    },
+    fontFamily: "'Oswald', sans-serif",
     color: "#F8D800",
     background: "linear-gradient(to right, #000000, #434343)", //zapożyczone z https://cssgradient.io/
   },
   light: {
+    fontFamily: "'Oswald', sans-serif",
     color: "#0D25B9",
     background: "linear-gradient(to right, #ffefba, #ffffff)", //zapożyczone z https://cssgradient.io/
   },
--- src/userDetals/UserDetals.jsx
+++ src/userDetals/UserDetals.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
+import {useTheme} from '../bgContext/BgContext';
+import "./userDetals.css"
 
 const UserDetals = ({value}) =>{
+    const [error, setError] = useState(false)
     const history = useNavigate()
+    const { theme } = useTheme() //dane z BgContext
+    const [toggleBg, setToggleBg] = useState(false) //do weryfikacji obecnego tła
 
+    useEffect(()=>{
+        if(value.length !==1){
+        setError(true)
+        }
+        },[value])
+        
+    const change=()=> {
+        setToggleBg(current => !current)
+    }
+    console.log(value);
     return(
-        <div className="userDetalsContener">
+    <>
+        {error && <div style={theme.dark} className="error">
+            <h2>BAZA JEST OBECNIE PUSTA..<br />WRÓĆ DO WYSZUKIWANIA</h2>
+            <button onClick={()=>history("/")} className="btn errorBtn" type="button">ZAMKNIJ</button>
+            </div>}
+        {value && <div className="userDetalsContener" style={toggleBg ? theme.light : theme.dark}>
+            <div className="themeContener">
+            <button onClick={change} className="btn">ZMIEŃ MOTYW</button>
+            </div>
+            <div className="titleContener">
             {value.map(el=>(<h2 key={el.id}>DANE UŻYTKOWNIKA: {el.name.toUpperCase()}</h2>))}
-         {value.length === 1 ? value.map(el=>(
+            </div>
+         {value.map(el=>(
          <table key={el.id}>
                <thead>
                    <tr>
@@ -26,7 +51,7 @@ const UserDetals = ({value}) =>{
                </thead>
                <tbody>
                   <tr key={el.id}>
-                    <td>NICK: {el.username}, TELEFON: {el.phone}, MAIL: {el.email}, STRONA: {el.website}</td>
+                    <td>NICK: {el.username},TELEFON: {el.phone}, MAIL: {el.email}, STRONA: {el.website}</td>
                    </tr>
                </tbody>
                <thead>
@@ -39,9 +64,10 @@ const UserDetals = ({value}) =>{
                     <td>NAZWA FIRMY: {el.company.name},  BRANŻA: {el.company.bs}</td>
                    </tr>
                </tbody>
-           </table>)) : null}
+           </table>))}
         <button onClick={()=>history("/")} className="btn" type="button">ZAMKNIJ</button>
-        </div>
+        </div>}
+    </>
     )
 }
 
